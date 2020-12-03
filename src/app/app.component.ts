2dcd810ce269c17770c5ec8dd974c6d01c3bc4df
rmedrano <randalles007@yahoo.es>
2020-12-02 21:59:05 -0600

    using FormArray Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,64 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, NgModule } from '@angular/core';
+import { FormGroup, FormArray, FormBuilder, FormControl, ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'appTest';
+
+export class AppComponent implements OnInit {
+  titulo = 'Experiencia Laboral';
+  formulario: FormGroup;
+  changes: any;
+
+  get experienciaLaboral(): FormArray {
+    return this.formulario.get('experienciaLaboral') as FormArray;
+  }
+
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit() {
+    this.crearFormulario();
+    this.anadirExperienciaLaboral();
+    this.crearListeners();
+  }
+
+  crearFormulario() {
+    this.formulario = this.fb.group({
+      experienciaLaboral: this.fb.array([])
+    });
+  }
+
+  anadirExperienciaLaboral() {
+    debugger;
+    const trabajo = this.fb.group({
+      empresa: new FormControl(''),
+      puesto: new FormControl(''),
+      descripcion: new FormControl('')
+    });
+
+    this.experienciaLaboral.push(trabajo);
+  }
+
+  borrarTrabajo(indice: number) {
+    this.experienciaLaboral.removeAt(indice);
+  }
+  crearListeners() {
+    // this.forma.statusChanges.subscribe( status => console.log({ status }));
+    // this.formulario.get('experienciaLaboral').valueChanges.subscribe( console.log );        
+    this.formulario.get('experienciaLaboral').valueChanges.subscribe(values => {
+      this.changes = values;
+    });
+
+  }
+
+  PosteoFormulario() {
+    debugger;
+    (this.formulario.get('experienciaLaboral') as FormArray).valueChanges.subscribe(values => {
+      console.log(values);
+    });
+  }
+
 }
+
